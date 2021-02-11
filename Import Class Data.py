15916e359a3540fa2ab13acb31957446335574cc
Spyros <ssm737309@gmail.com>
2021-02-11 22:12:52 +0200

    Initial commit

---
+++ Import Class Data.py
@@ -0,0 +1,8 @@
+# Importing Class Data
+# cd = Class (A,B,C,D,E)
+# dm = Tmima (1,2,3,4,5)
+# y = Year
+# sp = Specialty
+# pl = Place / Town
+# sm = Semester (S = Spring or A = Autumn)
+cd = print (input("Please insert the Class Data"))
