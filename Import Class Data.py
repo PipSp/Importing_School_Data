445ae3851f6596e856a9b573c7f5c854de4091b8
Spyros <ssm737309@gmail.com>
2021-02-11 22:47:25 +0200

    Delete Import Class Data.py

--- Import Class Data.py
+++
@@ -1,8 +0,0 @@
-# Importing Class Data
-# cd = Class (A,B,C,D,E)
-# dm = Tmima (1,2,3,4,5)
-# y = Year
-# sp = Specialty
-# pl = Place / Town
-# sm = Semester (S = Spring or A = Autumn)
-cd = print (input("Please insert the Class Data"))
